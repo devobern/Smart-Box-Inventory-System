df0b8a528c627d4b808968613604a4ae02c992f4
Vejsa1 <vejsa1@bfh.ch>
2024-11-02 11:07:46 +0100

    Add: Define types used to store box, category and location

---
+++ code/app/types/category.ts
@@ -0,0 +1,5 @@
+export type category = {
+    id: bigint;                 // Unique ID
+    name: string;               // Name for a category
+    dateAdded?: Date;           // Date the category was added
+};
---
+++ code/app/types/location.ts
@@ -0,0 +1,5 @@
+export type location = {
+    id: bigint;                 // Unique ID
+    name: string;               // Name for a location
+    dateAdded?: Date;           // Date the location was added
+};
