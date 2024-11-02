2092b93c1dbaf985ce6e4301153f3b2e30f659d5
lueln1 <nathan.lue@students.bfh.ch>
2024-11-02 15:57:29 +0100

    New camera style (before wasn't working)

--- code/app/scanner/index.tsx
+++ code/app/scanner/index.tsx
@@ -1,15 +1,23 @@
-import {View} from "react-native";
+import { StyleSheet, SafeAreaView, View} from "react-native";
 import {CameraView} from "expo-camera";
+import { Stack } from "expo-router";
 
 export default function Index() {
     return (
-        <View>
+        <SafeAreaView style={StyleSheet.absoluteFillObject}>
+            <Stack.Screen
+                options={{
+                    title: 'Overview',
+                    headerShown: false,
+                }}
+            />
             <CameraView
+                style={StyleSheet.absoluteFillObject}
                 facing={"back"}
                 onBarcodeScanned={({data}) => {
                     console.log("Data", data);
                 }}
             />
-        </View>
+        </SafeAreaView>
     )
 }
