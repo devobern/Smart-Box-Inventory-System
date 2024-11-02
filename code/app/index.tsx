38a2a5d38c78c9b49037fe423284f017072ef83d
Lukas Gysin <55271228+testgoofy@users.noreply.github.com>
2024-11-02 19:03:21 +0100

    refactor: Remove unused code

--- code/app/index.tsx
+++ code/app/index.tsx
@@ -18,31 +18,6 @@ import {category} from "@/app/types/category";
 // Create tables if they don't exist
 db.createTables();
 
-const styles = StyleSheet.create({
-  item: {
-    justifyContent: "space-between",
-    borderColor: "#000000",
-    borderWidth: 1,
-    borderRadius: 10,
-    padding: 15,
-    marginVertical: 4,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-  nbItems: {
-    textAlign: "right",
-    fontSize: 14,
-    color: "gray",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    flex: 1,
-  },
-});
-
 export default function Index() {
 
    let boxes = [] as box[];
@@ -54,36 +29,6 @@ export default function Index() {
             });
         }
     });
-
-    const presetCategories = [
-        { name: 'Electronics' },
-        { name: 'Home Appliances' },
-        { name: 'Books' },
-        { name: 'Clothing' },
-        { name: 'Sports Equipment' },
-        { name: 'Beauty & Personal Care' },
-        { name: 'Toys & Games' },
-        { name: 'Furniture' },
-        { name: 'Groceries' },
-        { name: 'Automotive Parts' },
-    ];
-
-  const renderItem = ({ item }: any) => (
-        <View style={styles.item}>
-            <TouchableOpacity onPress={() => router.push(`/boxes/details?id=${item.id}`)}>
-                <View style={styles.titleContainer}>
-                    <Text style={styles.title}>{item.title}</Text>
-                </View>
-                <Text style={styles.nbItems}>({item.nb_items})</Text>
-            </TouchableOpacity>
-        </View>
-    );
-
-    const addPresetCategories = () => {
-        for (let category of presetCategories) {
-            const id = db.addCategory(category.name);
-        };
-    }
 
   return (
     <View
