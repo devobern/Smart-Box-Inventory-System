4ca0e11c9e64a9938585f535c18f27892e666e94
Nicolin Dora <nicolin@nicolin-dora.ch>
2024-11-02 13:44:05 +0100

    feat: Replace top bar with SearchHeader component in index screen

--- code/app/index.tsx
+++ code/app/index.tsx
@@ -1,19 +1,24 @@
 import { Link } from "@react-navigation/native";
 import { Text, View } from "react-native";
+import SearchHeader from "../components/SearchHeader";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link to="/boxes">
-        List of Boxes
-      </Link>
+    <View style={{ flex: 1 }}>
+      {/* Replace top bar with SearchHeader */}
+      <SearchHeader />
+      {/* Main content */}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: 20,
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Link to="/boxes">List of Boxes</Link>
+      </View>
     </View>
   );
 }
