bf72fbb1ac6f359b0e24718d7ffca2fd1558b9df
Matt Daniel Josef <daniel.matt@students.bfh.ch>
2024-11-02 13:42:52 +0100

    Box list component and add box view

--- code/app/boxes/add.tsx
+++ code/app/boxes/add.tsx
@@ -1,19 +1,72 @@
-import { Link } from "@react-navigation/native";
-import { Text, View } from "react-native";
+import {Button, StyleSheet, TextInput, Text, View, Pressable} from "react-native";
+import {useState} from "react";
+import {router} from "expo-router";
+
+
 
 export default function AddBox() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Add a new box</Text>
-      <Link to="/boxes">
-        Back to list
-      </Link>
-    </View>
-  );
-}
+    //Replace with type object Box.ts
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [creationDate, setcDate] = useState(new Date())
+    const [updateDate, setuDate] = useState(new Date())
+
+    // TO DO: Add function for save on db
+    // db.addBox().then(router.push('/boxes'));
+
+    return (
+        <View>
+            <Text style={styles.text}>Name</Text>
+            <TextInput
+                placeholder="Name"
+                value={name}
+                onChangeText={setName}
+                style={styles.inputText}
+            />
+            <Text style={styles.text}>Description</Text>
+            <TextInput
+                placeholder="Description"
+                value={description}
+                onChangeText={setDescription}
+                style={styles.inputText}
+            />
+            <Text style={styles.text}>Location</Text>
+            <Pressable style={styles.saveButton} onPress={()=>{
+                // TO DO: Save new box in the DB
+                router.push('/boxes')
+            }}>
+                <Text style={styles.saveText}>Save</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    inputText: {
+        borderWidth: 1,
+        marginBottom: 10,
+        margin: 10,
+        padding: 10,
+        borderRadius: 10
+    },
+    text: {
+        marginBottom: 1,
+        padding: 10
+    },
+    saveButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+    },
+    saveText: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
+});
