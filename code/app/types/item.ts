c9402cc97c0b158ba7bf4c55003c592e253cb4d0
Vejsa1 <vejsa1@bfh.ch>
2024-11-02 11:08:42 +0100

    Update: Remove unused attribute, add id attribute

--- code/app/types/item.ts
+++ code/app/types/item.ts
@@ -1,9 +1,9 @@
 export type Item = {
-    name: string;               // Unique name for each item
+    id: bigint;                 // Unique id
+    name: string;               // Name for each item
     category: string;           // Category of the item, e.g., "Clothing", "Electronics"
     boxId: string;              // Identifier for the box storing this item
     description?: string;       // Optional detailed description about the item
-    location?: string;          // General location where the box is stored -> FOR FUTURE IMPLEMENTATION
     quantity?: number;          // Number of identical items -> FOR FUTURE IMPLEMENTATION
     photoUrl?: string;          // URL to a photo of the item -> FOR FUTURE IMPLEMENTATION
     dateAdded?: Date;           // Date the item was added -> FOR FUTURE IMPLEMENTATION
